80d62477c53580fd20cf9e058cfb9c98b961720d
andy <arteysoft@gmail.com>
2023-06-03 12:32:59 -0300

    primer commit clase #1

---
+++ clase1/src/main/java/edu/it/controllers/AlumnosController.java
@@ -0,0 +1,20 @@
+package edu.it.controllers;
+
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+public class AlumnosController extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1>Hora Actual: ");
+            out.println(System.currentTimeMillis());
+            out.println("</h1>");
+            response.setStatus(200);
+    }
+}
---
+++ clase1/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hola Rapidos y Furiosos X</h2>
+</body>
+</html>
