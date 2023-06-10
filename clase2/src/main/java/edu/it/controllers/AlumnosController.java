5841acca9844b52ab1f4e14ed787ce77a457c835
andy <arteysoft@gmail.com>
2023-06-10 11:00:50 -0300

    trabajo practico

--- clase2/src/main/java/edu/it/controllers/AlumnosController.java
+++ clase2/src/main/java/edu/it/controllers/AlumnosController.java
@@ -4,17 +4,30 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import edu.it.components.Utiles;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 
 public class AlumnosController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-            response.setContentType("text/plain");
+            response.setContentType("Application/json");
             PrintWriter out = response.getWriter();
-            out.println("<h1>Hora Actual: ");
-            out.println(System.currentTimeMillis());
-            out.println("</h1>");
+            
+            /*
+             * Van a generar 5 alumnos, los ponene en un array o ArrayList
+             * Luego lo mismo, lo transforman a Json
+             */
+            
+            var alumno = Utiles.generarAlumnoRandom();
+            var alumnoJson = new Gson().toJson(alumno);
+            
+            out.print(alumnoJson);
+            
             response.setStatus(200);
     }
 }
---
+++ clase2/src/main/java/edu/it/model/Alumno.java
@@ -0,0 +1,27 @@
+package edu.it.model;
+
+
+public class Alumno {
+    public String id;
+    public String nombre;
+    public String apellido;
+    public String calle;
+    public String calleNumero;
+    public String estado;
+    public String pais;
+    
+    public Alumno() {
+    }
+
+	public Alumno(String id, String nombre, String apellido, String calle, String calleNumero, String estado,
+			String pais) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.calle = calle;
+		this.calleNumero = calleNumero;
+		this.estado = estado;
+		this.pais = pais;
+	}
+}
