4b5454d977e53ab4fba70d0b7d0b908d5f2b95c8
andy <arteysoft@gmail.com>
2023-06-10 12:59:16 -0300

    clase #2

--- clase2/src/main/java/edu/it/controllers/AlumnosController.java
+++ clase2/src/main/java/edu/it/controllers/AlumnosController.java
@@ -8,8 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import edu.it.components.Utiles;
+import edu.it.model.Alumno;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.servlet.ServletException;
 
 public class AlumnosController extends HttpServlet {
@@ -23,11 +27,63 @@ public class AlumnosController extends HttpServlet {
              * Luego lo mismo, lo transforman a Json
              */
             
-            var alumno = Utiles.generarAlumnoRandom();
+            var alumno = new Alumno[] {Utiles.generarAlumnoRandom(), Utiles.generarAlumnoRandom(), Utiles.generarAlumnoRandom()};
             var alumnoJson = new Gson().toJson(alumno);
             
             out.print(alumnoJson);
             
             response.setStatus(200);
     }
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+    		
+    		try {
+    				var isreader = new InputStreamReader(request.getInputStream());
+    				var buffReader = new BufferedReader(isreader);
+    				
+    				StringBuilder strBld = new StringBuilder();
+    				
+    				for (String linea = buffReader.readLine();linea != null;linea = buffReader.readLine()) {
+    					strBld.append(linea);
+    				}
+    				
+    				System.out.println(strBld.toString());
+    				
+    				var alumno = new Gson().fromJson(strBld.toString(), Alumno.class);
+    				
+    				// Como ponemos esto en una base de datos MySQL
+    				
+    				response.setStatus(201);
+    		}
+    		catch (Exception ex) {
+    			response.setStatus(500);
+    		}
+    }
+    public void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+    		
+    		try {
+    				var isreader = new InputStreamReader(request.getInputStream());
+    				var buffReader = new BufferedReader(isreader);
+    				
+    				StringBuilder strBld = new StringBuilder();
+    				
+    				for (String linea = buffReader.readLine();linea != null;linea = buffReader.readLine()) {
+    					strBld.append(linea);
+    				}
+    				
+    				System.out.println(strBld.toString());
+    				
+    				var alumno = new Gson().fromJson(strBld.toString(), Alumno.class);
+    				
+    				// Como ponemos esto en una base de datos MySQL
+    				
+    				response.setStatus(200);
+    		}
+    		catch (Exception ex) {
+    			response.setStatus(500);
+    		}
+    }
+    
+    
 }
